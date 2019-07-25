cf03f340d09659f37598852b18a098f76714c4a1
haihoing <haihoing@qq.com>
2019-07-25 21:30:52 +0800

    ZZIPlib-master.tar.gz 采用这个版本。

--- CMake/Dependencies.cmake
+++ CMake/Dependencies.cmake
@@ -128,19 +128,19 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
 
     message(STATUS "Building ZZIPlib")
     file(DOWNLOAD
-        https://cmway.hawkhai.com/gradle/zziplib-develop.tar.gz
-        ${PROJECT_BINARY_DIR}/zziplib-develop.tar.gz)
+        https://cmway.hawkhai.com/gradle/ZZIPlib-master.tar.gz
+        ${PROJECT_BINARY_DIR}/ZZIPlib-master.tar.gz)
     execute_process(COMMAND ${CMAKE_COMMAND}
-        -E tar xf zziplib-develop.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+        -E tar xf ZZIPlib-master.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
     execute_process(COMMAND ${BUILD_COMMAND_COMMON}
         -DZLIB_ROOT=${OGREDEPS_PATH}
         -DZZIPMMAPPED=OFF -DZZIPCOMPAT=OFF -DZZIPLIBTOOL=OFF -DZZIPFSEEKO=OFF -DZZIPWRAP=OFF -DZZIPSDL=OFF -DZZIPBINS=OFF -DZZIPTEST=OFF -DZZIPDOCS=OFF -DBASH=sh
         -DBUILD_STATIC_LIBS=TRUE
         -DBUILD_SHARED_LIBS=${OGREDEPS_SHARED}
-        ${PROJECT_BINARY_DIR}/zziplib-develop
-        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/zziplib-develop)
+        ${PROJECT_BINARY_DIR}/ZZIPlib-master
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/ZZIPlib-master)
     execute_process(COMMAND ${CMAKE_COMMAND} 
-        --build ${PROJECT_BINARY_DIR}/zziplib-develop ${BUILD_COMMAND_OPTS})
+        --build ${PROJECT_BINARY_DIR}/ZZIPlib-master ${BUILD_COMMAND_OPTS})
 
     message(STATUS "Building pugixml")
     file(DOWNLOAD
